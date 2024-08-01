ca4440d7f1649ea4a415f06e2367ccc045a3dede
Johan Klingström <johanklingstrom@anotherblock.io>
2024-08-01 22:38:26 +0200

    update, shader material

--- main.js
+++ main.js
@@ -3,23 +3,41 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+const aspect = width / height;
+const camera = new THREE.OrthographicCamera(-aspect, aspect, 1, -1, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1);
+const material = new THREE.ShaderMaterial({
+  vertexShader: /* glsl */`
+    varying vec2 v_texcoord;
+    void main() {
+        gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        v_texcoord = uv;
+    }`,
+  fragmentShader: /* glsl */`
+    varying vec2 v_texcoord;
+    void main() {
+        vec2 st = v_texcoord;
+        vec3 color = vec3(st.x, st.y, 1.0);
+        gl_FragColor = vec4(color.rgb, 1.0);
+    }`
+});
 const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
 
-const temp = {
-  width: 1024,
-  height: 720
-}
+renderer.setSize(width, height);
 
-const camera = new THREE.PerspectiveCamera(75, temp.width / temp.height);
+camera.position.z = 1;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(temp.width, temp.height);
-document.body.appendChild(renderer.domElement);
-
-camera.position.z = 4;
-
-renderer.render(scene, camera);
+const animate = () => {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+};
+animate();
